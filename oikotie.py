a658ab8814c6521aadad6ce76f0ee3db207d52a1
dangquangdon <dangquangdon@gmail.com>
2019-01-09 10:49:04 +0200

    more house info

--- oikotie.py
+++ oikotie.py
@@ -9,22 +9,47 @@ cards = soup.findChild('div', class_="cards")
 
 tags = cards.findAll('div', class_="cards__card ng-scope")
 
-# for tag in tags:
-#     street_address = tag.findChild('div', class_="ot-card__address").div.text
+for tag in tags:
+    street_address = tag.findChild('div', class_="ot-card__address").div.text
 
-#     area = tag.findChild('div', class_="ot-card__address").findAll('span')
-#     if len(area) > 1:
-#         distric = area[0].text
-#         city = area[1].text
-#     elif len(area) == 1:
-#         city = area[0].text
-#     else:
-#         city = "No info"
+    area = tag.findChild('div', class_="ot-card__address").findAll('span')
+    if len(area) > 1:
+        distric = area[0].text
+        city = area[1].text
+    elif len(area) == 1:
+        city = area[0].text
+    else:
+        city = "No info"
 
-#     print("========")
-#     print(street_address)
-#     print(distric)
-#     print(city)
-#     print("++++++++")
+    house_info = tag.findChild('div', class_="ot-card__body").findAll('section')
+    
+    house_price = house_info[0].findAll('span')[1].text
 
-print(tags[0].findChild('div', class_="ot-card__body"))
+    house_area = house_info[0].findAll('span')[2].text
+    try:
+        house_rooms = house_info[1].text
+    except IndexError:
+        house_rooms = "No Info"
+
+    try:
+        house_type = house_info[2].findAll('span')[0].text
+    except IndexError:
+        house_type = "No Info"
+
+    try:    
+        house_year = house_info[2].findAll('span')[1].text
+    except IndexError:
+        house_year = "No Info"
+
+    print("========")
+    print(street_address)
+    print(distric)
+    print(city)
+    print(house_price)
+    print(house_area)
+    print(house_rooms)
+    print(house_type)§
+    print(house_year)
+    print("++++++++")
+
+# print(tags[0].findChild('div', class_="ot-card__body").findAll('section')[2].findAll('span'))
